802ddc0dc288b7c4969f460c95e4f0a07c34a3e2
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-22 21:17:30 +0300

    ready by date

--- src/test/java/TestDEMOQA.java
+++ src/test/java/TestDEMOQA.java
@@ -1,14 +1,28 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestDEMOQA {
+    @BeforeAll
+    static void BeforeAll(){
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+
     @Test
     void testDemoqaFullForm() {
-        Configuration.pageLoadStrategy = "eager";
+            open("https://demoqa.com/automation-practice-form");
+            $("[id=firstName]").setValue("Ivan");
+            $("[id=lastName]").setValue("Ivanov");
+            $("[id=userEmail]").setValue("IIvanov@gmail.com");
+            $(byText("Male")).click();
+            $("[id=userNumber]").setValue("+123456789");
 
 
 
