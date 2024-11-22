52e89428e8315a905558029bc3424afe0c6d9035
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-22 14:45:26 +0300

    inition commit

---
+++ src/test/java/TestDEMOQA.java
@@ -0,0 +1,16 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestDEMOQA {
+    @Test
+    void testDemoqaFullForm() {
+        Configuration.pageLoadStrategy = "eager";
+
+
+
+    }
+}
