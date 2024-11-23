77306cf2e6302bfd5eacf4f938f7a37941ab36d3
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-23 18:26:59 +0300

    all form without calendar

--- src/test/java/TestDEMOQA.java
+++ src/test/java/TestDEMOQA.java
@@ -1,6 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -18,11 +21,44 @@ public class TestDEMOQA {
     @Test
     void testDemoqaFullForm() {
             open("https://demoqa.com/automation-practice-form");
+
             $("[id=firstName]").setValue("Ivan");
+
             $("[id=lastName]").setValue("Ivanov");
+
             $("[id=userEmail]").setValue("IIvanov@gmail.com");
+
             $(byText("Male")).click();
-            $("[id=userNumber]").setValue("+123456789");
+
+            $("[id=userNumber]").setValue("0123456789");
+
+
+
+            //$("[id=dateOfBirthInput]").click();
+            //$("[id=dateOfBirthInput]").setValue("21 Nov 2024");
+
+            $("[id=subjectsInput]").setValue("Computer Science").sendKeys(Keys.TAB);
+            $("[id=subjectsInput]").setValue("Commerce").sendKeys(Keys.TAB);
+            $(byText("Sports")).click();
+
+            File fileToUpload = new File("src/test/resources/Screen Form.png");
+            $("[id=uploadPicture]").uploadFile(fileToUpload);
+
+            $("[id=currentAddress]").setValue("Some street 1");
+
+            $("[id=state]").click();
+            $(byText("Rajasthan")).click();
+            $("[id=city]").click();
+            $(byText("Jaiselmer")).click();
+
+      /*      $("[id=dateOfBirthInput]").click();
+            $("[class=react-datepicker__month-select]").selectOption("January");
+            $("[class=react-datepicker__year-select]").selectOption("2000");
+            $("[class=react-datepicker__day--001]").click();*/
+
+            $("[id=submit]").click();
+
+
 
 
 
