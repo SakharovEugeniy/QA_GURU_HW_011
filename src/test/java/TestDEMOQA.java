9458e9f1b37a510e30791134bd1039cdd00fe6c3
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-25 08:22:36 +0300

    final version 1.001

--- src/test/java/TestDEMOQA.java
+++ src/test/java/TestDEMOQA.java
@@ -22,6 +22,8 @@ public class TestDEMOQA {
     void testDemoqaFullForm() {
             open("https://demoqa.com/automation-practice-form");
 
+            //complete the form
+
             $("[id=firstName]").setValue("Ivan");
 
             $("[id=lastName]").setValue("Ivanov");
@@ -32,10 +34,10 @@ public class TestDEMOQA {
 
             $("[id=userNumber]").setValue("0123456789");
 
-
-
-            //$("[id=dateOfBirthInput]").click();
-            //$("[id=dateOfBirthInput]").setValue("21 Nov 2024");
+            $("[id=dateOfBirthInput]").click();
+            $("[class=react-datepicker__month-select]").selectOption("January");
+            $("[class=react-datepicker__year-select]").selectOption("2000");
+            $("[class~=react-datepicker__day--001]").click();
 
             $("[id=subjectsInput]").setValue("Computer Science").sendKeys(Keys.TAB);
             $("[id=subjectsInput]").setValue("Commerce").sendKeys(Keys.TAB);
@@ -51,16 +53,25 @@ public class TestDEMOQA {
             $("[id=city]").click();
             $(byText("Jaiselmer")).click();
 
-      /*      $("[id=dateOfBirthInput]").click();
-            $("[class=react-datepicker__month-select]").selectOption("January");
-            $("[class=react-datepicker__year-select]").selectOption("2000");
-            $("[class=react-datepicker__day--001]").click();*/
-
             $("[id=submit]").click();
 
 
+            //complete the form
 
+            $("[class=table-responsive]").$(byText("Student Name")).parent().shouldHave(text("Ivan Ivanov"));
 
+            //                                            НАПОМИНАЛКА
+            //$("[class=table-responsive]").$(byText("Student Name")).sibling(0).shouldHave(text("Ivan Ivanov"));
+            // Можно искать не в родительском эл-те а в родственном , sibling(0) первый родственный после найденного
 
+            $("[class=table-responsive]").$(byText("Student Email")).parent().shouldHave(text("IIvanov@gmail.com"));
+            $("[class=table-responsive]").$(byText("Gender")).parent().shouldHave(text("Male"));
+            $("[class=table-responsive]").$(byText("Mobile")).parent().shouldHave(text("0123456789"));
+            $("[class=table-responsive]").$(byText("Date of Birth")).parent().shouldHave(text("01 January,2000"));
+            $("[class=table-responsive]").$(byText("Subjects")).parent().shouldHave(text("Computer Science, Commerce"));
+            $("[class=table-responsive]").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+            $("[class=table-responsive]").$(byText("Picture")).parent().shouldHave(text("Screen Form.png"));
+            $("[class=table-responsive]").$(byText("Address")).parent().shouldHave(text("Some street 1"));
+            $("[class=table-responsive]").$(byText("State and City")).parent().shouldHave(text("Rajasthan Jaiselmer"));
     }
 }
